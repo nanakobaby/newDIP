5abd2c725121c978e4eec02cac7144a2e898a3d3
nanako kan <n.k.0213.0529@gmail.com>
2020-05-31 15:35:56 +0900

    agenda削除機能実装

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,5 @@
 class AgendasController < ApplicationController
-  # before_action :set_agenda, only: %i[show edit update destroy]
+  before_action :set_agenda, only: %i[destroy]
 
   def index
     @agendas = Agenda.all
@@ -19,6 +19,16 @@ class AgendasController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @article = Article.where(agenda_id: @agenda.id)
+    @article.each do |n|
+      n.destroy
+    end
+
+    @agenda.destroy
+    redirect_to dashboard_url, notice: I18n.t('views.messages.agenda_deleted')
   end
 
   private
